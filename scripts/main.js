eb4364afa04d241bda46d9af50e7c1cce6683ea7
DeumdeIgne <lachlan.a.p.1995@gmail.com>
2020-06-08 21:47:13 +1000

    Changing Pictures

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,23 @@ if (mapZone == 'Oceania') {
 } else {
     alert ('You are in Oceania');
 }
+function multiply (num1,num2) {
+    let result = num1 * num2;
+    return result;
+}
+alert (multiply(5,6));
+
+document.querySelector('h1').onclick = function() {
+    alert ("Maps of the World");
+}
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/map.jpeg') {
+        myImage.setAttribute ('src', 'images/Oceania.png');
+    } else {
+        myImage.setAttribute ('src', 'images/map.jpeg');
+    }
+}
